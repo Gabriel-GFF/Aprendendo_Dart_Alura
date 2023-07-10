eef389b198d7ede5f781cda46c7674f55aa507b5
Gabriel-GFF <gabrielfariasformal@gmail.com>
2023-07-10 16:11:14 -0300

    Commit atualização

---
+++ aulas/variavel_double.dart
@@ -0,0 +1,11 @@
+//Double = É um tipo de variavel de valores reais, numeros com virgulas.
+//Expressar um numero com exponenciais! Numeros muito grnade!!
+//Limite do double está na precisão do limite dos numeros, (Numero de casas depois da virgula, 54 casas)
+
+
+void main(){
+  int num = 26;
+  double altura = 12.5;
+  double exponencial = 780e10;
+  print('Altura: $altura, Num: $num, Expo: $exponencial');
+}
---
+++ aulas/variavel_int.dart
@@ -0,0 +1,11 @@
+//Int = São tipos de variaveis voltadas para o numero inteiro(numeros sem virgulas).
+
+//Só inicializa uma variavel de forma DIRETA se existir um valor a ser atribuido.
+//numeros como hexdexcial formato: 0x000000
+//Limite 4 drilhoes + e -
+
+void main(){
+  int idade = 26;//
+  int hex = 0x00001A;
+  print('$idade , $hex');
+}
